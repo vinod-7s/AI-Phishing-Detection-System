e87f4f502664a8cd3619479c8e94808b5ab3ac35
ambulgerushabh45-star <ambulgerushabh45@gmail.com>
2026-07-16 01:45:11 +0530

    Frontend production configuration

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://ai-phishing-detection-system-wflj.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-
-  console.log("TOKEN SENT:", token);
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
